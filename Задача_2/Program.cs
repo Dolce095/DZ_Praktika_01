c789feb7d85c833cf19367e8cbe5b9694b7b7be8
Alexander Efremov <dolce095.ae@gmail.com>
2022-09-20 15:13:13 +0300

    DZ_1

---
+++ Задача_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.Write("Введите число 1, ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2, ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num1 > num2)
+    {
+            Console.Write("Число 1 - максимальное. \n\n");
+        }
+        else
+        {
+            Console.Write("Число 2 - максимальное. \n\n");
+        }
+     
---
+++ Задача_4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.Write("Введите число 1, ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2, ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3, ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+    {
+        if (num1 > num3)
+        {
+            Console.Write("Число 1 - максимальное. \n\n");
+        }
+        else
+        {
+            Console.Write("Число 3 - максимальное. \n\n");
+        }
+    }
+    else if (num2 > num3)
+        Console.Write("Число 2 - максимальное. \n\n");
+    else
+        Console.Write("Число 3 - максимальное. \n\n");
